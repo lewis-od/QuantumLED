1e406d000c5b81e26792c0b6d21aa124e9146523
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2019-08-11 19:42:07 +0100

    Remove num_using/NUMUSING

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -2,7 +2,6 @@
 #include <SPI.h>
 
 #define NUMPIXELS 72 // Number of LEDs in strip
-#define NUMUSING 20 // Number of LEDs to actually illuminate
 
 #define DATAPIN 4
 #define CLOCKPIN 5
@@ -10,11 +9,11 @@
 Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
 
 // Buffer for holding colours sent via serial
-char inputBuffer[NUMUSING * 3];
+char inputBuffer[NUMPIXELS * 3];
 
 void setup() {
   // Setup serial and DotStar strip
-  Serial.begin(9600);
+  Serial.begin(9800);
   strip.begin();
   strip.show(); // Turn all LEDs off
 }
@@ -22,10 +21,10 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     // Read colours from serial
-    Serial.readBytes(inputBuffer, NUMUSING * 3);
+    Serial.readBytes(inputBuffer, NUMPIXELS * 3);
     
     // Set LED colours
-    for (int i = 0; i < NUMUSING; i++) {
+    for (int i = 0; i < NUMPIXELS; i++) {
       strip.setPixelColor(i,
         // Note colours are sent as [R,G,B] but DotStar expects [G,R,B]
         int(inputBuffer[i*3 + 1]), int(inputBuffer[i*3]), int(inputBuffer[i*3 + 2])
